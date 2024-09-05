5cc8281f6baf68ae2a49702ff4b7c1611e7629b3
BurkeStrang <burke.strang@outlook.com>
2024-09-05 14:16:29 -0400

    echo

--- HttpServer/Domain/Models/HttpRequest.cs
+++ HttpServer/Domain/Models/HttpRequest.cs
@@ -4,14 +4,22 @@ public class HttpRequest
 {
     public string? Method { get; set; }
     public string? Path { get; set; }
+    public string? Body { get; set; }
 
     public static HttpRequest Parse(byte[] bufferRequest)
     {
+
         string requestString = Encoding.ASCII.GetString(bufferRequest);
         string startLine = requestString.Split("\r\n")[0];
         string method = startLine.Split(" ")[0];
         string path = startLine.Split(" ")[1];
+        string? body = path.Split("/")[^1] ?? null;
 
-        return new HttpRequest { Method = method, Path = path };
+        return new HttpRequest
+        {
+            Method = method,
+            Path = path,
+            Body = body,
+        };
     }
 }
--- HttpServer/Domain/Models/HttpResponse.cs
+++ HttpServer/Domain/Models/HttpResponse.cs
@@ -4,16 +4,39 @@ public class HttpResponse
 {
     public int StatusCode { get; set; }
     public string? ReasonPhrase { get; set; }
+    public string? Body { get; set; }
 
     public override string ToString()
     {
-        return $"HTTP/1.1 {StatusCode} {ReasonPhrase}\r\n\r\n";
+        if (Body is not null)
+        {
+            string headers = $"Content-Type: text/plain\r\nContent-Length: {Body.Length}";
+            return $"HTTP/1.1 {StatusCode} {ReasonPhrase}\r\n{headers}\r\n\r\n{Body}";
+        }
+        else
+        {
+            return $"HTTP/1.1 {StatusCode} {ReasonPhrase}\r\n\r\n";
+        }
     }
 
     public static HttpResponse CreateResponse(HttpRequest request)
     {
-        return request.Path!.Equals("/")
-            ? new HttpResponse { StatusCode = 200, ReasonPhrase = "OK" }
-            : new HttpResponse { StatusCode = 404, ReasonPhrase = "Not Found" };
+        if (request.Path!.Equals("/"))
+        {
+            return new HttpResponse { StatusCode = 200, ReasonPhrase = "OK" };
+        }
+        else if (request.Path.StartsWith("/echo/"))
+        {
+            return new HttpResponse
+            {
+                StatusCode = 200,
+                ReasonPhrase = "OK",
+                Body = request.Body,
+            };
+        }
+        else
+        {
+            return new HttpResponse { StatusCode = 404, ReasonPhrase = "Not Found" };
+        }
     }
 }
