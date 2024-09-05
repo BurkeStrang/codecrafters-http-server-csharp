9d2f6a5d0e8f09233982bbe4c43207af1b349c6c
BurkeStrang <burke.strang@outlook.com>
2024-09-05 15:36:02 -0400

    async

--- HttpServer/Infrastructure/SocketHandler.cs
+++ HttpServer/Infrastructure/SocketHandler.cs
@@ -2,16 +2,16 @@ namespace codecrafters_http_server.HttpServer.Infrastructure;
 
 public class SocketHandler
 {
-    public static void HandleClient(Socket client)
+    public async static Task HandleClient(Socket client)
     {
         byte[] bufferRequest = new byte[1024];
-        client.Receive(bufferRequest);
+        await client.ReceiveAsync(bufferRequest);
 
         HttpRequest request = HttpRequest.Parse(bufferRequest);
         HttpResponse response = HttpResponse.CreateResponse(request);
 
         byte[] msg = Encoding.ASCII.GetBytes(response.ToString());
-        client.Send(msg);
+        await client.SendAsync(msg);
 
         // Console.WriteLine(response.ToString());
     }
